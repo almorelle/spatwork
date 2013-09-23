11bb8d3bd38edfcbf3ef86ec193ffa355a8c90c5
Alexis Morelle <alexis.morelle@gmail.com>
2013-09-23 14:08:35 +0200

    game ctrl - no goal/end

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -61,3 +61,21 @@ var rankCtrl = controllers.controller("RankCtrl", function($scope, Restangular){
     });
     $scope.predicate = ['-wins', '-goals'];
 });
+
+var gameCtrl = controllers.controller("GameCtrl", function($scope, Restangular){
+    Restangular.all("players").getList().then(function(players){
+        $scope.players = players;
+        Restangular.all("games").getList().then(function(games){
+            $scope.game = games[0];
+            for(var i=0;i<$scope.players.length;i++){
+                var player = $scope.players[i];
+                player.isPlaying = ((_.indexOf($scope.game.teamA.teammateRefs, player._id) !== -1) ||
+                                    (_.indexOf($scope.game.teamB.teammateRefs, player._id) !== -1));
+            }
+        }, function errorCallback() {
+            alert("Oops unable to get info from server. Please refresh. :(");
+        });
+    }, function errorCallback() {
+        alert("Oops unable to get info from server. Please refresh. :(");
+    });
+});
