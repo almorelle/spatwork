b7cbfc8dfba52522204c7314ffe437b4ba867705
Alexis Morelle <alexis.morelle@gmail.com>
2013-11-13 17:18:30 +0100

    rankingPoints service

--- src/main/webapp/js/controllers/rankCtrl.js
+++ src/main/webapp/js/controllers/rankCtrl.js
@@ -1,10 +1,9 @@
-var rankCtrl = controllers.controller("RankCtrl", function($scope, Restangular){
+var rankCtrl = controllers.controller("RankCtrl", function($scope, rankingPoints, Restangular){
     Restangular.all("players").getList().then(function(players){
         $scope.players = players;
         for(var i=0;i<$scope.players.length;i++){
         	$scope.players[i].played = $scope.players[i].wins + $scope.players[i].draws + $scope.players[i].losses;
-        	$scope.players[i].points = $scope.players[i].played == 0 ? 0 :
-        	    (($scope.players[i].wins*3) + $scope.players[i].draws) + ($scope.players[i].goals / $scope.players[i].played);
+            $scope.players[i].points = rankingPoints.computePoints($scope.players[i]);
         }
     }, function errorCallback() {
         alert("Oops unable to update server. Please refresh. :(");
--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -1,3 +1,12 @@
 'use strict';
 
 /* Services */
+var services = angular.module('spatworkServices', []);
+
+// Ranking points computing service.
+// Current formula is Points = ( ( 3 x V + N ) / Matchs ) + ( Goals / Matchs ) or 0 if never played.
+services.service('rankingPoints', function() {
+    this.computePoints = function(player) {
+        return player.played == 0 ? 0 : (((player.wins*3) + player.draws) / player.played) + (player.goals / player.played);
+    };
+});
