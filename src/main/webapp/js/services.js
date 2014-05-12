b9139f03617be57813f328bbff898b178b3daa51
Alexis Morelle <alexis.morelle@gmail.com>
2014-05-12 18:06:25 +0200

    added goals per match computation as a service

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -6,8 +6,12 @@ var services = angular.module('spatworkServices', []);
 // Ranking points computing service.
 // Current formula is Points = ( 3 x V + N ) + ( Goals x 0,05 ).
 services.service('rankingService', function() {
-    this.computePoints = function(player) {
+    this.points = function(player) {
         return ((player.wins*3) + player.draws) + (player.goals * 5e-2);
+    };
+
+    this.goalsPerMatch = function(player) {
+        return (player.played == 0) ? 0 : (player.goals / player.played);
     };
 });
 
