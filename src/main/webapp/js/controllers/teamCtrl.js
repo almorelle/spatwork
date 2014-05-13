9672d4e36c103b2cc37186cf763a648464f7e6e3
Alexis Morelle <alexis.morelle@gmail.com>
2014-05-13 16:19:40 +0200

    added balancing teams choice of algo and display of team stats

--- src/main/webapp/js/controllers/teamCtrl.js
+++ src/main/webapp/js/controllers/teamCtrl.js
@@ -14,23 +14,31 @@ var teamCtrl = controllers.controller("TeamCtrl", function($scope, rankingServic
             Restangular.all("games").getList().then(function(games){
                 $scope.game = _.findWhere(games,{finished: false});
                 $scope.game.teamA.totalgpm = 0;
+                $scope.game.teamA.totalpts = 0;
                 $scope.game.teamB.totalgpm = 0;
+                $scope.game.teamB.totalpts = 0;
                 for(var i=0;i<$scope.players.length;i++){
                     var player = $scope.players[i];
                     if(_.contains($scope.game.teamA.teammateRefs, player._id)){
                         player.teamRef = "A";
                         $scope.game.teamA.totalgpm += player.gpm;
+                        $scope.game.teamA.totalpts += player.points;
                         player.scored = _.reduce($scope.game.teamA.scorersRefs, function(memo, scorerRef){ return player._id == scorerRef ? memo+1 : memo; }, 0);
                         player.ownGoals = _.reduce($scope.game.teamB.scorersRefs, function(memo, scorerRef){ return player._id == scorerRef ? memo+1 : memo; }, 0);
                     } else if (_.contains($scope.game.teamB.teammateRefs, player._id)){
                         player.teamRef = "B";
                         $scope.game.teamB.totalgpm += player.gpm;
+                        $scope.game.teamB.totalpts += player.points;
                         player.scored = _.reduce($scope.game.teamB.scorersRefs, function(memo, scorerRef){ return player._id == scorerRef ? memo+1 : memo; }, 0);
                         player.ownGoals = _.reduce($scope.game.teamA.scorersRefs, function(memo, scorerRef){ return player._id == scorerRef ? memo+1 : memo; }, 0);
                     } else {
                         player.teamRef = "N";
                     }
                 }
+                $scope.game.teamA.averagegpm = $scope.game.teamA.totalgpm / $scope.game.teamA.teammateRefs.length;
+                $scope.game.teamA.averagepts = $scope.game.teamA.totalpts / $scope.game.teamA.teammateRefs.length;
+                $scope.game.teamB.averagegpm = $scope.game.teamB.totalgpm / $scope.game.teamB.teammateRefs.length;
+                $scope.game.teamB.averagepts = $scope.game.teamB.totalpts / $scope.game.teamB.teammateRefs.length;
             }, function errorCallback() {
                 alert("Oops unable to get info from server. Please refresh. :(");
             });
@@ -42,13 +50,17 @@ var teamCtrl = controllers.controller("TeamCtrl", function($scope, rankingServic
     $("[data-toggle='tooltip']").tooltip({placement: "top"});
 
     //Reset the scoring parameters and open up the goal form.
-    $scope.balanceTeams = function(){
+    $scope.balanceTeams = function(algo){
         if($scope.game.teamA.score == 0 && $scope.game.teamB.score == 0){
             //balance teams only if the game has not started yet
             $('#updateFade').modal('toggle');
             var editGame = Restangular.copy($scope.game);
-            var players = _.filter($scope.players, function(player){return _.indexOf(editGame.teamA.teammateRefs, player._id) != -1 || _.indexOf(editGame.teamB.teammateRefs, player._id) != -1;});
-            players = _.sortBy(players, "gpm");
+            var players = _.filter($scope.players, function(player)
+                        {
+                            return _.indexOf(editGame.teamA.teammateRefs, player._id) != -1 ||
+                                _.indexOf(editGame.teamB.teammateRefs, player._id) != -1;
+                        });
+            players = _.sortBy(players, algo);
 
             //balancing teams
             var pointsA = 0;
